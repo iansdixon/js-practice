38ff3b3f66ba7f9927782c843337750d5ebf451d
Ian Dixon <redashian03@gmail.com>
2018-10-11 19:38:32 -0700

    add 2nd dice

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ function init () {
   roundScore = 0;
   activePlayer = 0;
 
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
@@ -49,16 +50,19 @@ init();
   document.querySelector('.btn-roll').addEventListener('click', function() {
     if (gamePlaying) {
       //random #
-      var dice = Math.floor(Math.random()*6) +1;
+      var diceA = Math.floor(Math.random()*6) +1;
+      var diceB = Math.floor(Math.random()*6) +1;
 
       //display result
-      var diceDOM = document.querySelector('.dice');
-      diceDOM.style.display = 'block';
-      diceDOM.src = 'dice-' + dice + '.png';
+      document.getElementById('dice-1').style.display = 'block';
+      document.getElementById('dice-2').style.display = 'block';
+      document.getElementById('dice-1').src = 'dice-' + diceA + '.png';
+      document.getElementById('dice-2').src = 'dice-' + diceB + '.png';
+
 
       //update round score if # is not 1
-      if (dice !== 1) {
-        roundScore += dice;
+      if (diceA !== 1 && diceB !== 1) {
+        roundScore += diceA + diceB;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
       } else {
         nextPlayer();
@@ -74,9 +78,10 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
     //check if player won game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       document.querySelector('#name-' +activePlayer).textContent = 'Winner!';
-      document.querySelector('.dice').style.display = 'none';
+      document.getElementById('dice-1').style.display = 'none';
+      document.getElementById('dice-2').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 
